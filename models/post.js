4bdde3e5c1012ac2c1385b19728cc9aac50ea5cd
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2024-04-22 12:21:15 -0300

    stretch challenge - created/updated at

--- models/post.js
+++ models/post.js
@@ -3,7 +3,9 @@ const { Schema, model } = require('mongoose');
 const postSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
-  summary: { type: String, required: true }
+  summary: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
 module.exports = model('Post', postSchema);
