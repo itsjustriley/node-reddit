57e4691309ad77962b9a4e4a4eba0191cda06a52
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2024-04-22 14:03:50 -0300

    users can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,4 +19,10 @@ module.exports = app => {
       return res.redirect('/');
     });
   });
+
+  // LOGOUT
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    return res.redirect('/');
+  });
 }
